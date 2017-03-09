282d5b15970595d442520c87fd976a39cd58321a
starrpatty28 <starrpatty28@gmail.com>
2017-03-09 15:26:11 -0800

    add images to assests folder. Add sky, mountain and sun image to storyboard and put constraints on all images. Add a IUScrollView image and placed constraints on scroll view

---
+++ PageTheScroll/PageTheScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Noi-Ariella Baht Israel on 3/9/17.
+//  Copyright © 2017 Plant A Seed of Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
